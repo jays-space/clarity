320a73bac6fe305562896c62b19dd6f8090e7a9a
Jay Mondlana <contact.jays.space@gmail.com>
2023-02-16 14:10:38 +0200

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-4xl font-cursive">Cupcake Factory</h1>
+      <p className="text-xs font-raleway">MADE AT HOME</p>
+
+      <h3 className="font-raleway">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum,
+        rerum possimus. Nulla nemo ratione temporibus laborum iusto sunt eum
+        assumenda.
+      </h3>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      cursive: ["Babylonica", "cursive"],
+      raleway: ["Raleway", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
